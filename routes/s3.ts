c42e485b8d1ccc3644acf92626a075560f6cfab4
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2024-02-20 20:20:09 +0100

    test

--- routes/s3.ts
+++ routes/s3.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import S3Service from "../services/s3.ts";
-import { Request, Response, NextFunction } from "express";
 
 const S3Router = Router();
 
@@ -16,15 +15,5 @@ S3Router.route('/bucket/:bucketName/objects/:objectKey')
 
 S3Router.get('/bucket/:bucketName/objects/:objectKey/details', S3Service.getBucketObjDetails);
 S3Router.post('/bucket/:bucketName/objects/:objectKey/copy', S3Service.copyBucketObject);
-
-S3Router.post('/robot/state', async function(req: Request, res: Response, next: NextFunction) {
-  try {
-    console.log('req.body: ', req.body);
-    res.json(true);
-
-  } catch (error) {
-    next(error);
-  }
-});
 
 export { S3Router };
